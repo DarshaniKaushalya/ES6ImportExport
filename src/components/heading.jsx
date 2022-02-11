f3d7db7f94d3274889dcf31654ab40f1a6c62290
DarshaniKaushalya <darshanikaushalya7788dkd@gmail.com>
2022-02-11 17:35:50 +0530

    Initial commit

---
+++ src/components/heading.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+const pi = "3.14";
+
+export default pi;
---
+++ src/math.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const pi = "3.14";
+
+function doublePi() {
+  return pi * 2;
+}
+function thriplePi() {
+  return pi * 3;
+}
+
+export default pi;
+export { doublePi, thriplePi };
